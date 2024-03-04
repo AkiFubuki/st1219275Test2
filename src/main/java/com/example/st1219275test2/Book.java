7cf2d2af2f15ac5759779297bfaff2a428427926
AkiFubuki7673 <tong.anton0625@gmail.com>
2024-03-04 13:28:48 -0500

    1a complete

---
+++ src/main/java/com/example/st1219275test2/Book.java
@@ -0,0 +1,49 @@
+package com.example.st1219275test2;
+
+public class Book {
+    public String title;
+    public String author;
+    public String ISBN;
+    public String category;
+    public double price;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
